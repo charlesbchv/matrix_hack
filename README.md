6c1024f69be9a78b5824798db0930c9d61de4022
Charles <charles.batchaev@teleric.net>
2024-03-06 17:06:23 +0100

    Enjoy matrix berzloy

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# matrix_hack
-MatrixHackBorzScreen
+# Matrix KR Script
+
+Experience the iconic Matrix digital rain in your terminal, now with a twist of Korean and Japanese characters! `matrix_kr.sh` is a Bash script that brings the enthralling Matrix effect to life, right on your screen.
+
+## Getting Started
+
+Follow these simple steps to unleash the Matrix effect in your terminal.
+
+### Prerequisites
+
+Ensure you have a Bash shell environment to run the script. Most Unix-based systems like Linux and macOS come with Bash pre-installed.
+
+### Installation
+
+1. **Make the Script Executable**
+
+   Before running the script, you need to make it executable. Open your terminal and navigate to the directory containing `matrix_kr.sh`. Then, execute the following command:
+
+   ```bash
+   chmod +x matrix_kr.sh
+   ```
+
+   This command changes the script's permissions, allowing it to be run as an executable file.
+
+2. **Run the Script**
+
+   With the permissions set, you're ready to start the Matrix effect. Run the script by entering:
+
+   ```bash
+   ./matrix_kr.sh
+   ```
+
+   Prepare to be immersed in a cascade of Korean and Japanese characters, creating a mesmerizing Matrix-like digital rain in your terminal.
+
+## Stopping the Script
+
+To exit the Matrix and return to reality, simply press `Ctrl+C`. This key combination will stop the script and bring you back to your regular terminal prompt.
+
+Enjoy the digital deluge!
---
+++ matrix_carton_sepia.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Effet Matrix avec des lettres coréennes ou japonaises en Bash
+
+# Initialisation du terminal
+clear
+
+# Cacher le curseur pour un meilleur effet
+tput civis
+
+# Définir les couleurs
+green="\033[0;32m"
+light_green="\033[1;32m"
+default="\033[0m"
+
+# Obtenir la largeur et la hauteur du terminal
+width=$(tput cols)
+height=$(tput lines)
+
+# Créer un tableau pour stocker les positions de départ des colonnes
+declare -A start_columns
+
+# Initialiser la position de départ de chaque colonne à une ligne aléatoire
+for ((i=1; i<=width; i++)); do
+    start_columns[$i]=$(($RANDOM % height))
+done
+
+# Chaîne de caractères contenant des lettres coréennes et japonaises
+# Pour les lettres coréennes, utiliser l'intervalle U+AC00 à U+D7A3 (Hangul Syllables)
+# Pour les lettres japonaises, utiliser des Hiraganas ou des Kanjis par exemple
+# Ici, un mélange simple de quelques caractères coréens et japonais est utilisé
+characters="가나다라마바사아자차카타파하ひらがなカタカナ"
+
+# Fonction pour dessiner l'effet Matrix
+
+#draw_matrix() {
+#    for ((i=1; i<=width; i++)); do
+#        # Décider aléatoirement si un nouveau caractère doit être démarré dans la colonne
+#        if (( $RANDOM % 2 )); then
+#            # Déplacer le curseur à la position de départ de la colonne
+#            tput cup ${start_columns[$i]} $i
+#            
+#            # Décider aléatoirement de la couleur du caractère
+#            if (( $RANDOM % 2 )); then
+#                echo -en $green
+#            else
+#                echo -en $light_green
+#            fi
+#
+#            # Afficher un caractère aléatoire
+#            echo -n ${characters:RANDOM%${#characters}:1}
+#            
+#            # Mettre à jour la position de départ pour la colonne
+#            start_columns[$i]=$(( (start_columns[$i] + 1) % height ))
+#
+#            # Réinitialiser la couleur
+#            echo -en $default
+#        fi
+#    done
+#}
+
+# Fonction pour dessiner l'effet Matrix
+draw_matrix() {
+    local i c
+    for ((i=1; i<=width; i++)); do
+        # Choisir un caractère aléatoire
+        c=${characters:RANDOM%${#characters}:1}
+        
+        # Afficher un caractère aléatoire en couleur
+        echo -en "$color$c"
+    done
+}
+
+# Boucle principale pour dessiner continuellement l'effet Matrix
+while true; do
+    # Appeler draw_matrix et afficher chaque ligne sans nouvelle ligne
+    echo -ne "$(draw_matrix)\r"
+done
+
+# Boucle principale pour dessiner continuellement l'effet Matrix
+while true; do
+    draw_matrix
+    # Contrôler la vitesse de l'effet
+    #sleep 0.00000000000000000000000000000000000001
+done
+
+# Réinitialiser le terminal (en cas de sortie du script)
+tput cnorm  # Montrer le curseur
+clear
